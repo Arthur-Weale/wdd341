5e312b0a88fcdef1c1498108a434fd16453652f1
Arthur Weale <ajayrossy340@gmail.com>
2025-05-09 04:26:52 +0200

    Routes creation successful

---
+++ routes/mainRoute.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+const {getDatabase} = require("../controller/proffessional")
+
+router.get("/proffessional", async(req, res)=>{
+    try{
+        const response = await getDatabase()
+        console.log(response)
+        res.send(response)
+    }catch(e){
+        console.error("Error fetching data", e)
+        res.status(500).send("Something went wrong")
+    }    
+})
+
+module.exports = router
